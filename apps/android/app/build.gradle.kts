7b83eec43c3bdc220fa1fde1e7aaf1aa2212108f
ivanarena <ivan.x28x@gmail.com>
2026-05-20 16:21:04 +0200

    Bump Android release version to 1.0.3

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -135,8 +135,8 @@ android {
     applicationId = "com.author"
     minSdk = 26
     targetSdk = 37
-    versionCode = 3
-    versionName = "1.0.2"
+    versionCode = 4
+    versionName = "1.0.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

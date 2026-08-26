7a4914b2fda92d66d94c0b0aa0d3290dd719b8fd
ivanarena <ivan.x28x@gmail.com>
2026-08-26 12:56:17 +0200

    Prepare Android v1.0.19

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -152,8 +152,8 @@ android {
     applicationId = "com.author"
     minSdk = 26
     targetSdk = 37
-    versionCode = 18
-    versionName = "1.0.18"
+    versionCode = 19
+    versionName = "1.0.19"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
